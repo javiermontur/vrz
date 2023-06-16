4e10c52fce8943e0877d8ee67de3df73d07f7c92
Montero, Jose <jose.montero@accenture.com>
2023-06-16 16:57:11 -0600

    Utilities replacement

---
+++ blocks/promohero/promohero.js
@@ -0,0 +1,96 @@
+export default async function decorate(block) {
+  const columns = block.querySelectorAll(':scope > div > div');
+
+  if (columns.length == 2) {
+    block.classList.add('section', 'aem-GridColumn', 'aem-GridColumn--default--12');
+    const columnsDecorated = document.createElement('div');
+    columnsDecorated.classList.add('Grid', 'height--xs', 'height--md', 'u-paddingTop--xs16', 'u-paddingRight--xs0', 'u-paddingBottom--xs16', 'u-paddingLeft--xs0', 'u-paddingTop--lg20', 'u-paddingRight--lg0', 'u-paddingBottom--lg20', 'u-paddingLeft--lg50', 'u-flexAlignContentStart--md');
+    
+    const typeTextThenImg = columns[1].querySelectorAll('picture').length;
+    let colText = '';
+    let colImage = '';
+    let h1 = '';
+    let h2 = '';
+    let text = '';
+    let tooltip = '';
+    let picture = '';
+    let cta = '';
+
+    if(typeTextThenImg) {
+      h1 = columns[0].querySelector('h1');
+      h2 = columns[0].querySelector('h2');
+      text = columns[0].querySelector('ul > li');
+      tooltip = columns[0].querySelectorAll('ul > li')[1];
+      cta = columns[0].querySelector('.btn-wrap');
+      picture = columns[1].querySelector('picture');
+    } else {
+      h1 = columns[1].querySelector('h1');
+      h2 = columns[1].querySelector('h2');
+      text = columns[1].querySelector('ul > li');
+      tooltip = columns[1].querySelectorAll('ul > li')[1];
+      cta = columns[1].querySelector('.btn-wrap');
+      picture = columns[0].querySelector('picture');
+    }
+
+    console.log(typeTextThenImg,h1,h2,text,tooltip,picture)
+    
+    colText = `
+      <div class="Col Col--xs12 Col--md6 Col--lg6 u-positionRelative--xs u-positionRelative--md content-wrap">
+        <div class="headlineWrap u-marginBottom--xs16 u-marginBottom--lg20">
+        ${h1 ? `
+          <h1 class="u-fontDisplay u-textLineHeightExtraTight u-marginBottom--0 u-textBold u-textLeft u-displayInline">
+            ${h1.innerHTML}
+          </h1>
+        ` : ``}
+        ${h2 ? `
+          <h2 class="u-fontDisplay u-textLineHeightExtraTight u-marginBottom--0 u-textBold u-textLeft u-displayInline">
+            ${h2.innerHTML}
+          </h2>
+        ` : ``}
+        </div>
+        ${text ? `
+          <p class="u-fontDisplay u-textLineHeightExtraTight u-text--xs16 u-text--md u-text--lg20 u-marginBottom--xs24 u-marginBottom--lg32 u-textRegular u-textLeft  ">
+            ${text.innerHTML}
+            ${tooltip ? `
+            <span class="tooltip">&ZeroWidthSpace;
+              <span class="vzrf-tooltip" data-tooltip-content="${tooltip.innerHTML}" data-tooltip-type="icon" data-aria-label="" data-cmp-vzrf-initialized="true">
+                <button class="Icon Icon--info u-textDecorationNone u-text--xs14 u-text--lg16" tabindex="0" aria-describedby="vzrf-tooltip-temp" aria-expanded="false" aria-label=""></button>
+              </span>
+            </span>
+            ` : ``}
+          </p>
+        `:``}
+        ${cta.outerHTML}
+      </div>
+    `;
+
+    colImage = `
+      <div class="Col Col--xs12 Col--md6 Col--lg6 u-positionRelative--xs u-positionRelative--md u-flex u-flexAlignItemsCenter--xs u-flexJustifyCenter--xs">
+        <picture class="u-displayInlineBlock">
+          ${picture.innerHTML}
+        </picture>  
+      </div>
+    `;
+
+    columnsDecorated.innerHTML = `
+      ${colText}
+      ${colImage}
+    `;
+
+    block.innerHTML = `
+      <section class="section_1180477413">
+        <div class="Container">
+          <div class="promoHero">
+            <div data-cmp="vzrf-promo-hero" data-cmp-img-just="${typeTextThenImg ? 'right' : 'left'}" data-cmp-text-m="topLeft" data-cmp-img-size="product" data-cmp-img-align-dt="center" data-cmp-img-align-m="center" data-cmp-header="regular" data-cmp-promo-bg-color="white">
+              <div class="cmp-wrapper">
+                <!-- Columns Decorated Here -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    `;
+
+    block.querySelector('.cmp-wrapper').append(columnsDecorated);
+  }
+}
--- blocks/tabnav/tabnav.js
+++ blocks/tabnav/tabnav.js
@@ -1,3 +1,90 @@
 export default async function decorate(block) {
-    console.log('decorate TabNav', block, [...block.children])
+  const listLinks = block.querySelectorAll('ul > li > a');
+
+  if (listLinks.length) {
+    block.classList.add('experienceFragment', 'experiencefragment', 'aem-GridColumn', 'aem-GridColumn--default--12');
+    const tabsListDecorated = document.createElement('ul');
+    tabsListDecorated.classList.add('Tabs-list');
+    tabsListDecorated.innerHTML = `
+      ${[...listLinks].map((link, index) => {
+        const isActive = index == 0;
+        return `
+          <li class="Tab ${isActive ? 'is-active' : ''}">
+            <a href="${link.href}" tabindex="0" 
+              ${isActive ? 'style="border-bottom-color:  !important" aria-current="true"' : ''}>
+              ${link.text}
+            </a>
+          </li>
+        `
+      }).join('')}
+    `;
+
+    block.innerHTML = `
+      <div class="aem-Grid aem-Grid--12 aem-Grid--default--12 ">
+        <div class="responsivegrid aem-GridColumn aem-GridColumn--default--12">
+          <div class="aem-Grid aem-Grid--12 aem-Grid--default--12 ">
+            <div class="section aem-GridColumn aem-GridColumn--default--12">
+              <section id="section_copy">
+                <div class="Container">
+                  <div class="tabNav">
+                    <nav aria-label="secondary">
+                      <div class="tab-nav tab-reduced ">
+                        <div class="Tabs u-showForMediumUp">
+                          <!-- Tab list Decorated -->
+                        </div>
+                        <form class="Form-group u-hideForMediumUp" autocomplete="off">
+                          <select role="combobox" class="Form-input" sitecat-cta="mobile nav">
+                            <option value="/deals/" selected="">
+                              Deals
+                            </option>
+                            <option value="/deals/smartphones/">
+                              Smartphone
+                            </option>
+                            <option value="/deals/iphone/">
+                              iPhone
+                            </option>
+                            <option value="/shop/online/free-cell-phones/">
+                              Free Phone
+                            </option>
+                            <option value="/smartphones/certified-pre-owned/">
+                              Certified pre-owned
+                            </option>
+                            <option value="/deals/products/">
+                              Accessory
+                            </option>
+                            <option value="/deals/prepaid/">
+                              Prepaid
+                            </option>
+                            <option value="/bring-your-own-device/">
+                              Bring your own device
+                            </option>
+                            <option value="/deals/5g-home-internet/">
+                              5G Home Internet
+                            </option>
+                            <option value="/deals/fios/">
+                              Fios
+                            </option>
+                          </select>
+                        </form>
+                      </div>
+                    </nav>
+                  </div>
+                  <div class="html">
+                    <style>
+                      @media screen and (min-width: 320px) and (max-width: 767px) {
+                        .tabNav {
+                          margin-top: 12px;
+                        }
+                      }
+                    </style>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
+      </div>
+    `
+    block.querySelector('.Tabs').append(tabsListDecorated);
   }
+}
